402e2d7d4d454be0c3fb8599ef8fe4d945ce548c
Thomas Gängler <tgaengler@avantgarde-labs.de>
2015-09-21 16:46:40 +0200

    added license + cleared dependencies a bit

--- src/test/java/org/dswarm/xmlenhancer/test/AaltoXmlTest.java
+++ src/test/java/org/dswarm/xmlenhancer/test/AaltoXmlTest.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (C) 2013 – 2015 SLUB Dresden & Avantgarde Labs GmbH (<code@dswarm.org>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.dswarm.xmlenhancer.test;
 
 import java.io.IOException;
@@ -14,14 +29,19 @@ import com.fasterxml.aalto.AsyncXMLStreamReader;
 import com.fasterxml.aalto.stax.InputFactoryImpl;
 import com.google.common.io.ByteSource;
 import com.google.common.io.Resources;
-import org.junit.Test;
 
 /**
  * @author tgaengler
  */
 public class AaltoXmlTest {
 
-	@Test
+	/**
+	 * does not work, just for investigation and evaluation
+	 *
+	 * @throws IOException
+	 * @throws XMLStreamException
+	 */
+	//@Test
 	public void aaltoXmlTest() throws IOException, XMLStreamException {
 
 		// http://fasterxml.github.io/stax2-api/javadoc/3.1.4/org/codehaus/stax2/XMLInputFactory2.html
@@ -135,7 +155,13 @@ public class AaltoXmlTest {
 		}
 	}
 
-	@Test
+	/**
+	 * does not work, just for investigation and evaluation
+	 *
+	 * @throws IOException
+	 * @throws XMLStreamException
+	 */
+	//@Test
 	public void aaltoXmlTest2() throws IOException, XMLStreamException {
 
 		final AsyncXMLInputFactory inputF = new InputFactoryImpl();
