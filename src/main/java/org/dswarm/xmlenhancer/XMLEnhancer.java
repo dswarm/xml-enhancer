37deecd7224a8a4c6346c1e2b71329ed90a50bbe
Thomas Gängler <thomas.gaengler@slub-dresden.de>
2016-09-05 15:28:07 +0200

    [DD-1378] don't unescape amp in attributes

--- src/main/java/org/dswarm/xmlenhancer/XMLEnhancer.java
+++ src/main/java/org/dswarm/xmlenhancer/XMLEnhancer.java
@@ -17,12 +17,15 @@ package org.dswarm.xmlenhancer;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 
-import org.apache.commons.io.Charsets;
+import org.apache.commons.lang3.StringEscapeUtils;
+import org.jsoup.nodes.Attributes;
+import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.nodes.Entities;
 import org.jsoup.nodes.Node;
@@ -47,7 +50,7 @@ public class XMLEnhancer {
 
 		final Path inputFilePath = Paths.get(inputFileName);
 		final byte[] inputBytes = Files.readAllBytes(inputFilePath);
-		final String inputString = new String(inputBytes, Charsets.UTF_8);
+		final String inputString = new String(inputBytes, StandardCharsets.UTF_8);
 
 		final List<Node> nodes = Parser.parseXmlFragment(inputString, DUMMY_BASE_URI);
 
@@ -58,29 +61,31 @@ public class XMLEnhancer {
 
 		final PrintWriter out = new PrintWriter(outResource, UTF_8);
 
-		nodes.forEach(node -> {
-
-			node.ownerDocument().outputSettings().escapeMode(Entities.EscapeMode.xhtml).preserveCase(true).prettyPrint(false);
-
-			if (node instanceof TextNode) {
-
-				final TextNode textNode = (TextNode) node;
-
-				final String wholeText = textNode.getWholeText();
-
-				out.print(wholeText);
-
-				return;
-			}
-
-			final String nodeString = node.toString();
-			final String unescapedNodeString = Parser.unescapeEntities(nodeString, true);
-
-			out.print(unescapedNodeString);
-		});
+		nodes.forEach(node -> unescapeEntity(out, node));
 
 		out.flush();
 		out.close();
+	}
+
+	private static void unescapeEntity(final PrintWriter out, final Node node) {
+
+		node.ownerDocument().outputSettings().escapeMode(Entities.EscapeMode.xhtml).syntax(Document.OutputSettings.Syntax.xml).prettyPrint(false);
+
+		if (node instanceof TextNode) {
+
+			final TextNode textNode = (TextNode) node;
+
+			final String wholeText = textNode.getWholeText();
+
+			out.print(wholeText);
+
+			return;
+		}
+
+		final String nodeString = node.toString();
+		final String unescapedNodeString = Parser.unescapeEntities(nodeString, true);
+
+		out.print(unescapedNodeString);
 	}
 
 	private static void enhanceNodes(final List<Node> nodes) {
@@ -91,6 +96,10 @@ public class XMLEnhancer {
 
 				Element element = (Element) node;
 
+				final Attributes attributes = element.attributes();
+
+				enhanceAttributes(attributes);
+
 				final List<Node> childNodes = element.childNodes();
 
 				enhanceNodes(childNodes);
@@ -100,29 +109,46 @@ public class XMLEnhancer {
 
 			if (node instanceof TextNode) {
 
-				final TextNode textNode = (TextNode) node;
-				final String wholeText = textNode.getWholeText();
-				final String text = node.toString();
-
-				if (text.trim().isEmpty()) {
-
-					return;
-				}
-
-				if (wholeText.startsWith(START_CDATA)) {
-
-					// do not add CDATA multiple times
-
-					return;
-				}
-
-				final String alignedText = alignTextWithWholeText(wholeText, text);
-
-				final String unescapeEntities = String.format("%s%s%s", START_CDATA, alignedText, END_CDATA);
-
-				textNode.text(unescapeEntities);
+				enhanceTextNode(node);
 			}
 		});
+	}
+
+	private static void enhanceAttributes(final Attributes attributes) {
+
+		attributes.forEach(attribute -> {
+
+			final String value = attribute.getValue();
+
+			String escapedValue = StringEscapeUtils.escapeXml11(value);
+
+			attribute.setValue(escapedValue);
+		});
+	}
+
+	private static void enhanceTextNode(final Node node) {
+
+		final TextNode textNode = (TextNode) node;
+		final String wholeText = textNode.getWholeText();
+		final String text = node.toString();
+
+		if (text.trim().isEmpty()) {
+
+			return;
+		}
+
+		if (wholeText.startsWith(START_CDATA)) {
+
+			// do not add CDATA multiple times
+
+			return;
+		}
+
+		final String alignedText = alignTextWithWholeText(wholeText, text);
+
+		final String unescapeEntities = String.format("%s%s%s", START_CDATA, alignedText, END_CDATA);
+
+		textNode.text(unescapeEntities);
 	}
 
 	private static String alignTextWithWholeText(final String wholeText, final String text) {
--- src/test/java/org/dswarm/xmlenhancer/test/XMLEnhancerTest.java
+++ src/test/java/org/dswarm/xmlenhancer/test/XMLEnhancerTest.java
@@ -18,13 +18,13 @@ package org.dswarm.xmlenhancer.test;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import com.google.common.io.ByteSource;
 import com.google.common.io.Resources;
-import org.apache.commons.io.Charsets;
 import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
@@ -43,7 +43,7 @@ public class XMLEnhancerTest {
 	public static final String TEMP_PREFIX = "temp_";
 
 	/**
-	 * we do not escaped non-escaped entities atm
+	 * we escape non-escaped entities atm, i.e., we do not keep content as it is
 	 *
 	 * @throws IOException
 	 */
@@ -61,7 +61,16 @@ public class XMLEnhancerTest {
 	public void xmlEnhancerTest3() throws IOException {
 
 		final String inputFileName = "nonEscapedAmp.xml";
-		final String expectedOutputFileName = "cdataed_nonEscapedAmp.xml";
+		final String expectedOutputFileName = "cdataed_escapedAmp.xml";
+
+		xmlEnhancerTestInternal(inputFileName, expectedOutputFileName);
+	}
+
+	@Test
+	public void xmlEnhancerTest4() throws IOException {
+
+		final String inputFileName = "nonEscapedAmp_2.xml";
+		final String expectedOutputFileName = "cdataed_nonEscapedAmp_2.xml";
 
 		xmlEnhancerTestInternal(inputFileName, expectedOutputFileName);
 	}
@@ -93,6 +102,15 @@ public class XMLEnhancerTest {
 		xmlEnhancerTestInternal(inputFileName, expectedOutputFileName);
 	}
 
+	@Test
+	public void jsoupTest5() throws IOException {
+
+		final String inputFileName = "testxml_w_amp_in_attr.xml";
+		final String expectedOutputFileName = "cdataed_testxml_w_amp_in_attr.xml";
+
+		xmlEnhancerTestInternal(inputFileName, expectedOutputFileName);
+	}
+
 	private void xmlEnhancerTestInternal(final String inputFileName, final String expectedOutputFileName) throws IOException {
 
 		final URL inputResourceURL = Resources.getResource(inputFileName);
@@ -113,8 +131,8 @@ public class XMLEnhancerTest {
 		Assert.assertNotNull(outBytes);
 		Assert.assertNotNull(expectedOutputBytes);
 
-		final String outString = new String(outBytes, Charsets.UTF_8);
-		final String expectedOutputString = new String(expectedOutputBytes, Charsets.UTF_8);
+		final String outString = new String(outBytes, StandardCharsets.UTF_8);
+		final String expectedOutputString = new String(expectedOutputBytes, StandardCharsets.UTF_8);
 
 		final Diff xmlDiff = DiffBuilder
 				.compare(Input.fromString(expectedOutputString).build())
